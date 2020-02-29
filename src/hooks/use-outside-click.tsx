a5ea49259735ec2a460429e7939ffd76df4d26d6
Matthew Moran <matthewtmoran@gmail.com>
2020-02-28 22:09:23 -0600

    remove mobile click effect from document

--- src/hooks/use-outside-click.tsx
+++ src/hooks/use-outside-click.tsx
@@ -2,9 +2,6 @@ import { useLayoutEffect } from "react"
 import { document } from "browser-monads"
 
 function useOutsideClick(ref, handler, when = true) {
-  if ("ontouchstart" in document.documentElement) {
-    document.body.style.cursor = "pointer"
-  }
   function handle(e) {
     if (ref && ref.current && !ref.current.contains(e.target)) {
       handler(e)
