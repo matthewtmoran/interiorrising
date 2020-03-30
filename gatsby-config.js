d44cdc39a8b4fe81477dd71a97fdf7b105d85e6b
Matthew Moran <matthewtmoran@gmail.com>
2020-03-30 15:01:03 -0500

    Remove unused deps

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,7 +70,6 @@ module.exports = {
         analyzerMode: "static",
       },
     },
-    `gatsby-plugin-sass`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
